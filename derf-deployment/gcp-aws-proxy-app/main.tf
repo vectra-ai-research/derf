3fa54ecb3a37a1670012f9ab7130b9c60dd9966d
Kat Traxler <ktraxler@vectra.ai>
2023-04-24 17:50:29 -0500

    -adding proxy app code

--- derf-deployment/gcp-aws-proxy-app/main.tf
+++ derf-deployment/gcp-aws-proxy-app/main.tf
@@ -3,37 +3,37 @@
 # DEPLOY A GOOGLE CLOUD STORAGE BUCKET AND UPLOAD PROXY APP CODE
 # ---------------------------------------------------------------------------------------------------------------------
 
-resource "random_uuid" "random" {
-}
-
-
-resource "google_storage_bucket" "derf-aws-proxy-app-bucket" {
- name          = "derf-aws-proxy-app-${random_uuid.random.result}"
- location      = "US"
- storage_class = "STANDARD"
-
- uniform_bucket_level_access = true
-}
-
-
-resource "google_storage_bucket_object" "derf-aws-proxy-app-folder" {
- name         = "derf-aws-proxy-app"
- source       = "OBJECT_PATH"
- bucket       = google_storage_bucket.derf-aws-proxy-app-bucket.id
-}
 
 # ---------------------------------------------------------------------------------------------------------------------
 # CREATE A CLOUD BUILD TRIGGER
 # ---------------------------------------------------------------------------------------------------------------------
 
 resource "google_cloudbuild_trigger" "aws_proxy_app_cloudbuild_trigger" {
-  location = "global"
-  description = "Cloud Source Repository Trigger ${local.repository_name} (${local.branch_name})"
+  location    = "global"
+  project     = local.gcp_deployment_project_id
+  description = "Github Trigger ${local.repository_name} (${local.branch_name})"
+  
+  github {
+    owner = "Vectra AI Research"
+    name  = "derf-vectra-private"
+    push {
+      branch = "^main$"
+    }
 
-  trigger_template {
-    branch_name = local.branch_name
-    repo_name   = local.repository_name
   }
+
+  git_file_source {
+    path      = "cloudbuild.yaml"
+    uri       = "https://github.com/vectra-ai-research/derf-vectra-private.git"
+    revision  = "refs/heads/main"
+    repo_type = "GITHUB"
+  }
+    approval_config {
+     approval_required = true 
+  }
+
+  include_build_logs = "INCLUDE_BUILD_LOGS_WITH_STATUS"
+
   ignored_files = ["README.md"]
 
   # These substitutions have been defined in the sample app's cloudbuild.yaml file.
@@ -51,8 +51,4 @@ resource "google_cloudbuild_trigger" "aws_proxy_app_cloudbuild_trigger" {
     _GCR_HOSTNAME         = local.gcr_hostname 
   }
 
-  # The filename argument instructs Cloud Build to look for a file in the root of the repository.
-  filename = "cloudbuild.yaml"
-
-  depends_on = [google_sourcerepo_repository.aws-proxy-app-repo]
 }
