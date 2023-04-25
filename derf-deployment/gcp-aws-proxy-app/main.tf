b913605b58a8c29fd912399235dd23550d903d2e
Kat Traxler <ktraxler@vectra.ai>
2023-04-25 16:21:57 -0500

    updated cloudbuild trigger

--- derf-deployment/gcp-aws-proxy-app/main.tf
+++ derf-deployment/gcp-aws-proxy-app/main.tf
@@ -14,8 +14,8 @@ resource "google_cloudbuild_trigger" "aws_proxy_app_cloudbuild_trigger" {
   description = "Github Trigger ${local.repository_name} (${local.branch_name})"
   
   github {
-    owner = "GoogleCloudPlatform"
-    name  = "cloud-run-microservice-template-python"
+    owner = "vectra-ai-research"
+    name  = "derf-vectra-private"
     push {
       branch = "^main$"
     }
@@ -23,31 +23,30 @@ resource "google_cloudbuild_trigger" "aws_proxy_app_cloudbuild_trigger" {
   }
 
   git_file_source {
-    path      = "Dockerfile"
-    uri       = "https://github.com/GoogleCloudPlatform/cloud-run-microservice-template-python.git"
+    path      = "derf-vectra-private /aws-proxy-app/cloudbuild.yaml"
+    uri       = "https://github.com/vectra-ai-research/derf-vectra-private.git"
     revision  = "refs/heads/main"
     repo_type = "GITHUB"
   }
-  #   approval_config {
-  #    approval_required = true 
-  # }
+    approval_config {
+     approval_required = true 
+  }
 
 
   ignored_files = ["README.md"]
 
-  # These substitutions have been defined in the sample app's cloudbuild.yaml file.
-  # See: https://github.com/robmorgan/sample-docker-app/blob/master/cloudbuild.yaml#L43
-  # substitutions = {
-  #   _LOCATION             = local.location
-  #   _GCR_REGION           = local.gcr_region
-  #   _SERVICE_NAME         = local.service_name
-  #   _PLATFORM             = "managed"
-  #   _SERVICE_NAME         = local.service_name
-  #   _TARGET_PROJECT       = local.source_project_id
-  #   _DEFAULT_COMPUTE_SA   =  local.default_compute_sa
-  #   _DEPLOY_REGION        = local.deploy_region
-  #   _SOURCE_PROJECT_ID    = local.source_project_id
-  #   _GCR_HOSTNAME         = local.gcr_hostname 
-  # }
+# Substitutions key names must begin with underscore and will swap out values in the cloudbuild yaml file.
+  substitutions = {
+    _LOCATION             = local.location
+    _GCR_REGION           = local.gcr_region
+    _SERVICE_NAME         = local.service_name
+    _PLATFORM             = "managed"
+    _SERVICE_NAME         = local.service_name
+    _TARGET_PROJECT       = local.source_project_id
+    _DEFAULT_COMPUTE_SA   =  local.default_compute_sa
+    _DEPLOY_REGION        = local.deploy_region
+    _SOURCE_PROJECT_ID    = local.source_project_id
+    _GCR_HOSTNAME         = local.gcr_hostname 
+  }
 
 }
