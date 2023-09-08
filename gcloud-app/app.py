8526bc0a655420e43f166ba4d799179e215e9e2f
Kat Traxler <kat@nanooksecurity.com>
2023-09-08 11:27:44 -0500

    updated gcloud app

--- gcloud-app/app.py
+++ gcloud-app/app.py
@@ -18,13 +18,18 @@ def validate_post():
     return update_users()
 
 
+
 @app.errorhandler(400)
 def bad_request(message):
   return jsonify(error=str(message)), 400
 
 def update_users():
-  update = subprocess.run(["gcloud", "run", "services" "update" "aws-proxy-app" "--update-secrets=AWS_ACCESS_KEY_ID_RSmith=derf-RSmith-accessKeyId-AWS:latest,AWS_SECRET_ACCESS_KEY_RSmith=derf-RSmith-accessKeySecret-AWS:latest" "region=us-central1" "--project $PROJECT_ID"],
-    stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+  PROJECT_ID = os.environ['--project=' + ['PROJECT_ID']]
+  update = subprocess.run(["gcloud", "run", "services", "update", "aws-proxy-app", "--update-secrets=AWS_ACCESS_KEY_ID_RSmith=derf-RSmith-accessKeyId-AWS:latest,AWS_SECRET_ACCESS_KEY_RSmith=derf-RSmith-accessKeySecret-AWS:latest", "region=us-central1", "PROJECT_ID"],
+    env=PROJECT_ID,
+    stdout=subprocess.PIPE, 
+    stderr=subprocess.PIPE,
+    capture_output=True,
     check=True,
     text=True)
   print(update.stderr)
