8b38d2a84cc276307696d908275b218340750fd5
Kat Traxler <kat@nanooksecurity.com>
2023-09-08 13:20:20 -0500

    updated gcloud app

--- gcloud-app/app.py
+++ gcloud-app/app.py
@@ -25,11 +25,12 @@ def bad_request(message):
 
 def update_users():
   data = request.json
-  projectFlag = "--project=" + os.environ['PROJECT_ID']
+  projectFlag = "--project " + os.environ['PROJECT_ID']
   updateSecrets = "--update-secrets=AWS_ACCESS_KEY_ID_" + data['NEWUSER'] + "=derf-" + data['NEWUSER'] + "-accessKeyId-AWS:latest,AWS_SECRET_ACCESS_KEY_" + data['NEWUSER'] + "=derf-" + data['NEWUSER'] "-accessKeySecret-AWS:latest"
   envVars = [projectFlag, updateSecrets]
-  update = subprocess.run(["gcloud", "run", "services", "update", "aws-proxy-app", "$updateSecrets", "region=us-central1", "$projectFlag"],
+  update = subprocess.run(["gcloud run services update aws-proxy-app $updateSecrets region=us-central1 $projectFlag"],
     env=envVars,
+    shell=True,
     stdout=subprocess.PIPE, 
     stderr=subprocess.PIPE,
     capture_output=True,
