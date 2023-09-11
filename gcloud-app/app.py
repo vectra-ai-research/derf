2dc582593e1c25664e3806591755c1234a433496
Kat Traxler <kat@nanooksecurity.com>
2023-09-10 22:23:34 -0500

    updated gcloud app

--- gcloud-app/app.py
+++ gcloud-app/app.py
@@ -18,7 +18,7 @@ def validate_post():
   if 'NEWUSER' not in data:
     abort(400, description='New User not specified')
   else:
-    return route_exec(data)
+    return updateSecrets(data)
 
 
 @app.errorhandler(400)
@@ -26,15 +26,15 @@ def bad_request(message):
   return jsonify(error=str(message)), 400
 
 
-def route_exec(data):
-    
+def updateSecrets(data):
+    projectId = os.os.environ['PROJECT_ID']
     newuser = data['NEWUSER']
     creds, project = google.auth.default( scopes=['googleapis.com/auth/cloud-platform'])
     gcloud_path = shutil.which("gcloud")
 
     try:
-        completedProcess = subprocess.run("$GCLOUD run services update aws-proxy-app '--update-secrets=AWS_ACCESS_KEY_ID_$NEWUSER=derf-$NEWUSER-accessKeyId-AWS:latest,AWS_SECRET_ACCESS_KEY_$NEWUSER=derf-$NEWUSER-accessKeySecret-AWS:latest' --region us-central1 --project derf-deployment-public", 
-                                          env={"GCLOUD": gcloud_path, "NEWUSER": newuser},
+        completedProcess = subprocess.run("$GCLOUD run services update aws-proxy-app '--update-secrets=AWS_ACCESS_KEY_ID_'$NEWUSER'=derf-'$NEWUSER'-accessKeyId-AWS:latest,AWS_SECRET_ACCESS_KEY_'$NEWUSER'=derf-'$NEWUSER'-accessKeySecret-AWS:latest' --region us-central1 --project $PROJECT_ID", 
+                                          env={"GCLOUD": gcloud_path, "NEWUSER": newuser, "PROJECT_ID": projectId},
                                           shell=True, 
                                           stdout=subprocess.PIPE, 
                                           stderr=subprocess.STDOUT, 
@@ -46,49 +46,8 @@ def route_exec(data):
     except subprocess.TimeoutExpired:
         response = print("Timedout", 400)
         return response
+    return response
 
 if __name__ == '__main__':
     app.run() 
 
-# def sample_update_service(data):
-#     # Create a client
-#     client = run_v2.ServicesClient()
-#     project_id = os.environ['PROJECT_ID']
-#     newuser = data['NEWUSER']
-
-#     # define a service request
-#         # define a service request
-#     request = run_v2.UpdateServiceRequest(
-#         service=run_v2.Service(
-#             name="projects/" + project_id + "/locations/us-central1/services/aws-proxy-app",
-#             template=run_v2.RevisionTemplate(
-#                 containers=[
-#                     run_v2.Container(
-#                         image="us-docker.pkg.dev/derf-artifact-registry-public/aws-proxy-app/aws-proxy-app:latest",
-#                         env=[
-#                             run_v2.EnvVar(
-#                                 name="AWS_ACCESS_KEY_ID_" + newuser,
-#                                 value_source=run_v2.EnvVarSource(
-#                                    secret_key_ref=run_v2.SecretKeySelector(
-#                                       secret="derf-" + newuser + "-accessKeyId-AWS",
-#                                       version="latest",
-#                                    ),
-#                                 ),
-#                             ),
-#                         ],
-#                     ),
-#                 ],
-#             ),
-#         ),
-#     )
-    
-#     # Make the request
-#     operation = client.update_service(request=request)
-
-#     print("Waiting for operation to complete...")
-
-#     response = operation.result()
-
-#     # Handle the response
-#     print(response)
-#     return(response.traffic_statuses)
