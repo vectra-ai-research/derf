a447abb3c22b8993d3735ba1ced72e97030b6d8b
Kat Traxler <kat@nanooksecurity.com>
2023-11-05 16:43:04 -0600

    updated ec2-execute-discovery-commands attack technique to use the correct UA

--- attack-techniques/aws/discovery/ec2-execute-discovery-commands/attack.tf
+++ attack-techniques/aws/discovery/ec2-execute-discovery-commands/attack.tf
@@ -31,7 +31,7 @@ resource "google_workflows_workflow" "workflow_to_invoke_ec2_execute_discovery_c
 ######################################################################################
 ## User Agent
 ######################################################################################
-#### Workflow executes with the User-Agent string: 
+#### Workflow (discovery commands within EC3) execute with the User-Agent string: 
 ##### "Derf-AWS-EC2-Execute-Discovery-Commands-SRT-WORKFLOWEXECUTIONID"
 
 ######################################################################################
@@ -51,6 +51,7 @@ main:
         assign:
         - user: $${args.user}
         - projectID: $${sys.get_env("GOOGLE_CLOUD_PROJECT_ID")} 
+        - executionId: '$${sys.get_env("GOOGLE_CLOUD_WORKFLOW_EXECUTION_ID")}'
     - getCloudRunURL:
         call: googleapis.run.v2.projects.locations.services.get
         args:
@@ -61,54 +62,63 @@ main:
         args:
             user: $${user}
             appEndpoint: $${appEndpoint.uri}
+            executionId: $${executionId}
         result: response1
     - S3LS:
         call: runS3LS
         args:
             user: $${user}
             appEndpoint: $${appEndpoint.uri}
+            executionId: $${executionId}
         result: response2
     - GetAccountSummary:
         call: runGetAccountSummary
         args:
             user: $${user}
             appEndpoint: $${appEndpoint.uri}
+            executionId: $${executionId}
         result: response3
     - ListRoles:
         call: runListRoles
         args:
             user: $${user}
             appEndpoint: $${appEndpoint.uri}
+            executionId: $${executionId}
         result: response4
     - ListUsers:
         call: runListUsers
         args:
             user: $${user}
             appEndpoint: $${appEndpoint.uri}
+            executionId: $${executionId}
         result: response5
     - GetAccountAuthorizationDetails:
         call: runGetAccountAuthorizationDetails
         args:
             user: $${user}
             appEndpoint: $${appEndpoint.uri}
+            executionId: $${executionId}
         result: response6        
     - DescribeSnaphots:
         call: runDescribeSnaphots
         args:
             user: $${user}
             appEndpoint: $${appEndpoint.uri}
+            executionId: $${executionId}
         result: response7
     - DescribeTrails:
         call: runDescribeTrails
         args:
             user: $${user}
             appEndpoint: $${appEndpoint.uri}
+            executionId: $${executionId}
         result: response8
     - ListDetectors:
         call: runListDetectors
         args:
             user: $${user}
             appEndpoint: $${appEndpoint.uri}
+            executionId: $${executionId}
         result: response9
     - createList:
         assign:
@@ -138,8 +148,13 @@ main:
 ## Submodules | Sub-Workflows
 ######################################################################################
 runGetCallerIdentity:
-  params: [appEndpoint, user]
+  params: [appEndpoint, user, executionId]
   steps:
+    - assign:
+        assign:
+        - body1: '{"InstanceIds": ["${local.instance_id}"], "DocumentName": "AWS-RunShellScript", "Parameters":{"commands": ["export AWS_EXECUTION_ENV=Derf-AWS-EC2-Execute-Discovery-Commands-SRT=='
+        - body2: '; aws sts get-caller-identity || true"]}}'
+        - body: '$${body1+executionId+body2}'
     - runGetCallerIdentity:
         call: http.post
         args:
@@ -153,19 +168,24 @@ runGetCallerIdentity:
               REGION: ${data.aws_region.current.name}
               SERVICE: "ssm" 
               ENDPOINT: "https://ssm.${data.aws_region.current.name}.amazonaws.com/"
-              BODY: '{"InstanceIds": ["${local.instance_id}"], "DocumentName": "AWS-RunShellScript", "Parameters":{"commands": ["export AWS_EXECUTION_ENV=Derf-AWS-EC2-Execute-Discovery-Commands-SRT; aws sts get-caller-identity || true"]}}'
+              BODY: '$${body}'
               CONTENT: "application/x-amz-json-1.1"
               TARGET: "AmazonSSM.SendCommand"
               VERB: POST
               USER: $${user}
-              UA: '$${"Derf-AWS-EC2-Execute-Discovery-Commands-SRT=="+sys.get_env("GOOGLE_CLOUD_WORKFLOW_EXECUTION_ID")}'
+              UA: 'Derf-AWS-EC2-Execute-Discovery-Commands'
         result: response
     - return:
         return: $${response.body.responseCode}
 
 runS3LS:
-  params: [appEndpoint, user]
+  params: [appEndpoint, user, executionId]
   steps:
+    - assign:
+        assign:
+        - body1: '{"InstanceIds": ["${local.instance_id}"], "DocumentName": "AWS-RunShellScript", "Parameters":{"commands": ["export AWS_EXECUTION_ENV=Derf-AWS-EC2-Execute-Discovery-Commands-SRT=='
+        - body2: '; aws s3 ls || true"]}}'
+        - body: '$${body1+executionId+body2}'
     - runS3LS:
         call: http.post
         args:
@@ -179,19 +199,24 @@ runS3LS:
               REGION: ${data.aws_region.current.name}
               SERVICE: "ssm" 
               ENDPOINT: "https://ssm.${data.aws_region.current.name}.amazonaws.com/"
-              BODY: '{"InstanceIds": ["${local.instance_id}"], "DocumentName": "AWS-RunShellScript", "Parameters":{"commands": ["export AWS_EXECUTION_ENV=Derf-AWS-EC2-Execute-Discovery-Commands-SRT; aws s3 ls || true"]}}'
+              BODY: '$${body}'
               CONTENT: "application/x-amz-json-1.1"
               TARGET: "AmazonSSM.SendCommand"
               VERB: POST
               USER: $${user}
-              UA: '$${"Derf-AWS-EC2-Execute-Discovery-Commands-SRT=="+sys.get_env("GOOGLE_CLOUD_WORKFLOW_EXECUTION_ID")}'
+              UA: 'Derf-AWS-EC2-Execute-Discovery-Commands'
         result: response
     - return:
         return: $${response.body.responseCode}
 
 runGetAccountSummary:
-  params: [appEndpoint, user]
+  params: [appEndpoint, user, executionId]
   steps:
+    - assign:
+        assign:
+        - body1: '{"InstanceIds": ["${local.instance_id}"], "DocumentName": "AWS-RunShellScript", "Parameters":{"commands": ["export AWS_EXECUTION_ENV=Derf-AWS-EC2-Execute-Discovery-Commands-SRT=='
+        - body2: '; aws iam get-account-summary || true"]}}'
+        - body: '$${body1+executionId+body2}'
     - runGetAccountSummary:
         call: http.post
         args:
@@ -205,19 +230,23 @@ runGetAccountSummary:
               REGION: ${data.aws_region.current.name}
               SERVICE: "ssm" 
               ENDPOINT: "https://ssm.${data.aws_region.current.name}.amazonaws.com/"
-              BODY: '{"InstanceIds": ["${local.instance_id}"], "DocumentName": "AWS-RunShellScript", "Parameters":{"commands": ["export AWS_EXECUTION_ENV=Derf-AWS-EC2-Execute-Discovery-Commands-SRT; aws iam get-account-summary || true"]}}'
-              CONTENT: "application/x-amz-json-1.1"
+              BODY: '$${body}'
               TARGET: "AmazonSSM.SendCommand"
               VERB: POST
               USER: $${user}
-              UA: '$${"Derf-AWS-EC2-Execute-Discovery-Commands-SRT=="+sys.get_env("GOOGLE_CLOUD_WORKFLOW_EXECUTION_ID")}'
+              UA: 'Derf-AWS-EC2-Execute-Discovery-Commands'
         result: response
     - return:
         return: $${response.body.responseCode}
 
 runListRoles:
-  params: [appEndpoint, user]
+  params: [appEndpoint, user, executionId]
   steps:
+    - assign:
+        assign:
+        - body1: '{"InstanceIds": ["${local.instance_id}"], "DocumentName": "AWS-RunShellScript", "Parameters":{"commands": ["export AWS_EXECUTION_ENV=Derf-AWS-EC2-Execute-Discovery-Commands-SRT=='
+        - body2: '; aws iam list-roles || true"]}}'
+        - body: '$${body1+executionId+body2}'
     - runListRoles:
         call: http.post
         args:
@@ -231,20 +260,25 @@ runListRoles:
               REGION: ${data.aws_region.current.name}
               SERVICE: "ssm" 
               ENDPOINT: "https://ssm.${data.aws_region.current.name}.amazonaws.com/"
-              BODY: '{"InstanceIds": ["${local.instance_id}"], "DocumentName": "AWS-RunShellScript", "Parameters":{"commands": ["export AWS_EXECUTION_ENV=Derf-AWS-EC2-Execute-Discovery-Commands-SRT; aws iam list-roles || true"]}}'
+              BODY: '$${body}'
               CONTENT: "application/x-amz-json-1.1"
               TARGET: "AmazonSSM.SendCommand"
               VERB: POST
               USER: $${user}
-              UA: '$${"Derf-AWS-EC2-Execute-Discovery-Commands-SRT=="+sys.get_env("GOOGLE_CLOUD_WORKFLOW_EXECUTION_ID")}'
+              UA: 'Derf-AWS-EC2-Execute-Discovery-Commands'
         result: response
     - return:
         return: $${response.body.responseCode}
 
 
 runListUsers:
-  params: [appEndpoint, user]
+  params: [appEndpoint, user, executionId]
   steps:
+    - assign:
+        assign:
+        - body1: '{"InstanceIds": ["${local.instance_id}"], "DocumentName": "AWS-RunShellScript", "Parameters":{"commands": ["export AWS_EXECUTION_ENV=Derf-AWS-EC2-Execute-Discovery-Commands-SRT=='
+        - body2: '; aws iam list-users || true"]}}'
+        - body: '$${body1+executionId+body2}'
     - runListUsers:
         call: http.post
         args:
@@ -258,20 +292,25 @@ runListUsers:
               REGION: ${data.aws_region.current.name}
               SERVICE: "ssm" 
               ENDPOINT: "https://ssm.${data.aws_region.current.name}.amazonaws.com/"
-              BODY: '{"InstanceIds": ["${local.instance_id}"], "DocumentName": "AWS-RunShellScript", "Parameters":{"commands": ["export AWS_EXECUTION_ENV=Derf-AWS-EC2-Execute-Discovery-Commands-SRT; aws iam list-users || true"]}}'
+              BODY: '$${body}'
               CONTENT: "application/x-amz-json-1.1"
               TARGET: "AmazonSSM.SendCommand"
               VERB: POST
               USER: $${user}
-              UA: '$${"Derf-AWS-EC2-Execute-Discovery-Commands-SRT=="+sys.get_env("GOOGLE_CLOUD_WORKFLOW_EXECUTION_ID")}'
+              UA: 'Derf-AWS-EC2-Execute-Discovery-Commands'
         result: response
     - return:
         return: $${response.body.responseCode}
 
 
 runGetAccountAuthorizationDetails:
-  params: [appEndpoint, user]
+  params: [appEndpoint, user, executionId]
   steps:
+    - assign:
+        assign:
+        - body1: '{"InstanceIds": ["${local.instance_id}"], "DocumentName": "AWS-RunShellScript", "Parameters":{"commands": ["export AWS_EXECUTION_ENV=Derf-AWS-EC2-Execute-Discovery-Commands-SRT=='
+        - body2: '; aws iam get-account-authorization-details >/dev/null || true"]}}'
+        - body: '$${body1+executionId+body2}'
     - runGetAccountAuthorizationDetails:
         call: http.post
         args:
@@ -285,20 +324,25 @@ runGetAccountAuthorizationDetails:
               REGION: ${data.aws_region.current.name}
               SERVICE: "ssm" 
               ENDPOINT: "https://ssm.${data.aws_region.current.name}.amazonaws.com/"
-              BODY: '{"InstanceIds": ["${local.instance_id}"], "DocumentName": "AWS-RunShellScript", "Parameters":{"commands": ["export AWS_EXECUTION_ENV=Derf-AWS-EC2-Execute-Discovery-Commands-SRT; aws iam get-account-authorization-details >/dev/null || true"]}}'
+              BODY: '$${body}'
               CONTENT: "application/x-amz-json-1.1"
               TARGET: "AmazonSSM.SendCommand"
               VERB: POST
               USER: $${user}
-              UA: '$${"Derf-AWS-EC2-Execute-Discovery-Commands-SRT=="+sys.get_env("GOOGLE_CLOUD_WORKFLOW_EXECUTION_ID")}'
+              UA: 'Derf-AWS-EC2-Execute-Discovery-Commands'
         result: response
     - return:
         return: $${response.body.responseCode}
 
 
 runDescribeSnaphots:
-  params: [appEndpoint, user]
+  params: [appEndpoint, user, executionId]
   steps:
+    - assign:
+        assign:
+        - body1: '{"InstanceIds": ["${local.instance_id}"], "DocumentName": "AWS-RunShellScript", "Parameters":{"commands": ["export AWS_EXECUTION_ENV=Derf-AWS-EC2-Execute-Discovery-Commands-SRT=='
+        - body2: '; aws ec2 describe-snapshots || true"]}}'
+        - body: '$${body1+executionId+body2}'
     - runDescribeSnaphots:
         call: http.post
         args:
@@ -312,19 +356,24 @@ runDescribeSnaphots:
               REGION: ${data.aws_region.current.name}
               SERVICE: "ssm" 
               ENDPOINT: "https://ssm.${data.aws_region.current.name}.amazonaws.com/"
-              BODY: '{"InstanceIds": ["${local.instance_id}"], "DocumentName": "AWS-RunShellScript", "Parameters":{"commands": ["export AWS_EXECUTION_ENV=Derf-AWS-EC2-Execute-Discovery-Commands-SRT; aws ec2 describe-snapshots || true"]}}'
+              BODY: '$${body}'
               CONTENT: "application/x-amz-json-1.1"
               TARGET: "AmazonSSM.SendCommand"
               VERB: POST
               USER: $${user}
-              UA: '$${"Derf-AWS-EC2-Execute-Discovery-Commands-SRT=="+sys.get_env("GOOGLE_CLOUD_WORKFLOW_EXECUTION_ID")}'
+              UA: 'Derf-AWS-EC2-Execute-Discovery-Commands'
         result: response
     - return:
         return: $${response.body.responseCode}
 
 runDescribeTrails:
-  params: [appEndpoint, user]
+  params: [appEndpoint, user, executionId]
   steps:
+    - assign:
+        assign:
+        - body1: '{"InstanceIds": ["${local.instance_id}"], "DocumentName": "AWS-RunShellScript", "Parameters":{"commands": ["export AWS_EXECUTION_ENV=Derf-AWS-EC2-Execute-Discovery-Commands-SRT=='
+        - body2: '; aws cloudtrail describe-trails || true"]}}'
+        - body: '$${body1+executionId+body2}'
     - runDescribeTrails:
         call: http.post
         args:
@@ -338,19 +387,24 @@ runDescribeTrails:
               REGION: ${data.aws_region.current.name}
               SERVICE: "ssm" 
               ENDPOINT: "https://ssm.${data.aws_region.current.name}.amazonaws.com/"
-              BODY: '{"InstanceIds": ["${local.instance_id}"], "DocumentName": "AWS-RunShellScript", "Parameters":{"commands": ["export AWS_EXECUTION_ENV=Derf-AWS-EC2-Execute-Discovery-Commands-SRT; aws cloudtrail describe-trails || true"]}}'
+              BODY: '$${body}'
               CONTENT: "application/x-amz-json-1.1"
               TARGET: "AmazonSSM.SendCommand"
               VERB: POST
               USER: $${user}
-              UA: '$${"Derf-AWS-EC2-Execute-Discovery-Commands-SRT=="+sys.get_env("GOOGLE_CLOUD_WORKFLOW_EXECUTION_ID")}'
+              UA: 'Derf-AWS-EC2-Execute-Discovery-Commands' 
         result: response
     - return:
         return: $${response.body.responseCode}
 
 runListDetectors:
-  params: [appEndpoint, user]
+  params: [appEndpoint, user, executionId]
   steps:
+    - assign:
+        assign:
+        - body1: '{"InstanceIds": ["${local.instance_id}"], "DocumentName": "AWS-RunShellScript", "Parameters":{"commands": ["export AWS_EXECUTION_ENV=Derf-AWS-EC2-Execute-Discovery-Commands-SRT=='
+        - body2: '; aws guardduty list-detectors || true"]}}'
+        - body: '$${body1+executionId+body2}'
     - runListDetectors:
         call: http.post
         args:
@@ -364,12 +418,12 @@ runListDetectors:
               REGION: ${data.aws_region.current.name}
               SERVICE: "ssm" 
               ENDPOINT: "https://ssm.${data.aws_region.current.name}.amazonaws.com/"
-              BODY: '{"InstanceIds": ["${local.instance_id}"], "DocumentName": "AWS-RunShellScript", "Parameters":{"commands": ["export AWS_EXECUTION_ENV=Derf-AWS-EC2-Execute-Discovery-Commands-SRT; aws guardduty list-detectors || true"]}}'
+              BODY: '$${body}'
               CONTENT: "application/x-amz-json-1.1"
               TARGET: "AmazonSSM.SendCommand"
               VERB: POST
               USER: $${user}
-              UA: '$${"Derf-AWS-EC2-Execute-Discovery-Commands-SRT=="+sys.get_env("GOOGLE_CLOUD_WORKFLOW_EXECUTION_ID")}'
+              UA: 'Derf-AWS-EC2-Execute-Discovery-Commands'
         result: response
     - return:
         return: $${response.body.responseCode}
